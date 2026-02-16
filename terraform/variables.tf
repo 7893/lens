c0db3b17347972e6b2757418ecdc3717f1613f30
7893 <8382299+7893@users.noreply.github.com>
2026-02-16 18:44:01 +0800

    chore: bind workers to terraform-provisioned resources

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "cloudflare_api_token" {
-  description = "Cloudflare API Token"
-  type        = string
-  sensitive   = true
-}
-
 variable "account_id" {
   description = "Cloudflare Account ID"
   type        = string
